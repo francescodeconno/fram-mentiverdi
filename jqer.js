5d3043fb0736a5c7bb0fa80d05ed34ebf9a6a3c3
Elecapp <41619251+Elecapp@users.noreply.github.com>
2021-01-30 00:49:57 +0100

    paly

--- jqer.js
+++ jqer.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-    
-$("#play").toggle(function(){$("#play").text('play');})
+
+$("#play").toggle(function(){$("#play").text('Play');})
+$("#play").toggle(function(){$("#play").text('Pause');})
+
+$(document).on('click', '.slideYThumbnailInside', function (event) {
+    var change = $(this).attr("src").split("/");
+    $("#stage > iframe").attr("src", "https://www.youtube.com/embed/"change[4]);
+            
+});   
+
 
   });
